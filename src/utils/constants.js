5978c91bc7a546f6e0210f6b5e7bf9b56ce0cb03
Cristiano <neivacristiano@yahoo.com.br>
2020-04-07 18:44:06 -0300

    creating constants for header

---
+++ src/utils/constants.js
@@ -0,0 +1,77 @@
+export const portalHeaderElements = {
+    menuElements: [
+        {
+            link: '/logIn',
+            text: 'LogIn'
+        },
+        {
+            link: '/cadastro',
+            text: 'Cadastro'
+        },
+        {
+            link: '#solucoes',
+            text: 'Serviços'
+        },
+        {
+            link: '#planos',
+            text: 'Planos'
+        }
+    ],
+    headerButtons: [
+        {
+            link: '/logIn',
+            text: 'Acesse sua Conta'
+        },
+        {
+            link: '#planos',
+            text: 'Planos'
+        },
+        {
+            link: '#solucoes',
+            text: 'Soluções'
+        },
+        {
+            link: '#ajuda',
+            text: 'Ajuda'
+        },
+    ]
+}
+
+export const cadastroHeaderElements = {
+    menuElements: [
+        {
+            link: '/logIn',
+            text: 'LogIn'
+        },
+        {
+            link: '/',
+            text: 'Portal'
+        },
+        {
+            link: '#planos',
+            text: 'Planos'
+        },
+        {
+            link: '/#ajuda',
+            text: 'Ajuda'
+        }
+    ],
+    headerButtons: [
+        {
+            link: '/',
+            text: 'Portal'
+        },
+        {
+            link: '#planos',
+            text: 'Planos'
+        },
+        {
+            link: '#register',
+            text: 'Registro'
+        },
+        {
+            link: '/#ajuda',
+            text: 'Ajuda'
+        },
+    ]
+}
